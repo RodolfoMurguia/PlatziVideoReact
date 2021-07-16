2fb085c8210b6b6131d96d280763cd2dcc38d1b6
Rodolfo Murguia <rmt.26.0978@gmail.com>
2021-07-16 11:24:35 -0500

    se agrega eslint y se modifica el gitignore

---
+++ src/components/Holamundo.jsx
@@ -0,0 +1,14 @@
+//importamos las librerias necesarias
+import React  from "react";
+
+//llamamos estilos
+import '../assets/styles/App.scss'
+
+const Holamundo = () => {
+
+    return(
+    <h1>Hola Mundo</h1>
+    )
+}
+
+export default Holamundo
---
+++ src/index.js
@@ -0,0 +1,9 @@
+//importo librerias
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//importo componentes
+import Holamundo from './components/Holamundo'
+
+//Generamos el renderer de react
+ReactDOM.render(<Holamundo/>, document.getElementById('app'))
